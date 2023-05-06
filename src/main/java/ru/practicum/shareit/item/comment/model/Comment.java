70f1f9240229513fdf755bc28b04f8a9303d4c2f
Maxim Kostyolov <kostyolov@yandex.ru>
2023-05-07 02:57:18 +0500

    Add controllers

---
+++ src/main/java/ru/practicum/shareit/item/comment/model/Comment.java
@@ -0,0 +1,40 @@
+package ru.practicum.shareit.item.comment.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Integer id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @OneToOne
+    @JoinColumn(name = "item_id", referencedColumnName = "item_id")
+    private Item item;
+
+    @OneToOne
+    @JoinColumn(name = "author_id", referencedColumnName = "user_id")
+    private User author;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime created;
+
+}
