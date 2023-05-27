1b170358ba6e89dbcc40b75d8a94b60bdd064f7a
Maxim Kostyolov <kostyolov@yandex.ru>
2023-05-27 21:32:58 +0500

    Add docker style:исправление codestyle

--- server/src/test/java/ru/practicum/shareit/booking/controller/BookingControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/controller/BookingControllerTest.java
@@ -8,7 +8,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Status;
